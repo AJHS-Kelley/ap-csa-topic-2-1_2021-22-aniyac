6cd6822517871240906c2fb137ad86da9df07192
aniyac <90394008+aniyac@users.noreply.github.com>
2021-11-05 09:40:25 -0400

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,118 @@
+import java.until.Scanner;
+
+  //unit 2, 11/05/21, version 0.4
+
+     public class student {
+     // these are INSTANCE VARIABLES.
+     Private int studentID;
+    private int gradeLevel;
+    private double gradepointAvg;
+    private String studentname;
+
+
+        // creatuing aconstructor
+        plubic student() {
+        this.studentID =0;
+        this.gradeLevel =0;
+        this.gradepointAvg =0.0;
+        this.studentname = "";
+        }
+
+     // value constructor 
+    public student(int studentID, int gradelevel, double GPA, string studentname) {
+        this.studentID = studentID;
+        this.studentName = studentName;
+         this.gradeLevel = 9;
+        this.gradepointAVG = 4.0;
+}
+    // more than one constructor is known as OVERLOADING.
+
+
+
+
+    // CREATING Methods
+
+    public static void calcGPA()
+        int grade0;
+        int grade1;
+        int garde2;
+        int grade3;
+        double newGPA;
+
+        Scanner myscanner = new scanner(system.in);
+
+        system.out. println("what is the frist grade?");
+        garde0 = myscanner.nextInt();
+        system.out.println("what is the second grade?");
+        grade1 = myscanner.nextInt();
+        system.out.println("what is the third grade?");
+        grade2 = myscanner.nextInt();
+        system.out.println("what is the fourth grade?");
+        grade3 = myscanner.nextInt();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //parameters ang arguements
+    public static void assignlunch(int gradelevel) {
+            o
